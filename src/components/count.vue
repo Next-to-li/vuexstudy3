f3096792bc3769a94ba5b21576eeb8eb4c7bd055
Next-to-li <1606707834@qq.com>
2018-09-10 01:33:55 +0800

    vuex02

---
+++ src/components/count.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h2>{{msg}}</h2>
+        <hr/>
+        <h3>{{$store.state.count}}-{{count}}</h3>
+        <p>
+            <button @click="$store.commit('add')">+</button>
+            <button @click="$store.commit('reduce')">-</button>
+        </p>
+    </div>
+</template>
+<script>
+    import store from '@/vuex/store'
+    import {mapState} from 'vuex';
+    export default{
+        data(){
+            return{
+                msg:'Hello Vuex',
+            }
+        },
+        /* computed: {
+            count(){
+                return this.$store.state.count; 
+            }  
+        }, */
+        /* computed:mapState({
+            count:state=>state.count
+        }), */
+       /*  computed:mapState(['count']), */
+        computed:mapState({
+            count:function(state){
+                return state.count;
+            }
+        }),
+        store
+    }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Count from '@/components/count'
+
 
 Vue.use(Router)
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/count',
+      component: Count
     }
+   
   ]
 })
